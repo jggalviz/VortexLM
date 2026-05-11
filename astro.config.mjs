3e8b683efbaab0a1e7e6ab5ac4dc08ea294454f9
VortexLM <dev@vortexlm.com>
2026-05-11 15:57:46 -0400

    fix: add missing index page, layouts and vercel ssr config

---
+++ astro.config.mjs
@@ -0,0 +1,11 @@
+import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
+import sitemap from '@astrojs/sitemap';
+import vercel from '@astrojs/vercel/serverless';
+
+export default defineConfig({
+  site: 'https://vortexlm.com',
+  output: 'server',
+  adapter: vercel(),
+  integrations: [tailwind(), sitemap()]
+});
